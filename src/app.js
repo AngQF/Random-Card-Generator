bd39b4ddacfb588d5844e0b3df8b9693ddfb6bc5
AngQF <angela.qf@hotmail.com>
2022-03-25 21:02:32 +0000

    cambios2

--- src/app.js
+++ src/app.js
@@ -7,36 +7,37 @@ const diamond = "♦";
 const spade = "♠";
 const club = "♣";
 const suits = [heart, diamond, spade, club];
-const values = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 const top = document.querySelector(".top-card");
 const bottom = document.querySelector(".bottom-card");
 const val = document.querySelector(".card-value");
 const buttonRandom = document.querySelector("#button1");
 
-const generateCard = (arr1, arr2) => {
-  let arrCards = "";
+const generateCard = () => {
+  let arrCards = [];
 
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      arrCards = arr1[i] + arr2[j];
-
-      top.innerHTML = bottom.innerHTML = arr1[i];
-      if (top.innerHTML === heart || top.innerHTML === diamond) {
-        top.style.color = bottom.style.color = "red";
-      } else {
-        top.style.color = bottom.style.color = "black";
-      }
-
-      val.innerHTML = arr2[j];
+  for (let i of suits) {
+    for (let j of values) {
+      let newCard = [i, j];
+      arrCards.push(newCard);
+      val.innerHTML = [j];
     }
-    return arrCards;
   }
+  let arrCardsRandom = Math.floor(Math.random() * arrCards.length);
+  let resultCards = arrCards[arrCardsRandom];
+
+  top.innerHTML = bottom.innerHTML = resultCards[0];
+  if (top.innerHTML === heart || top.innerHTML === diamond) {
+    top.style.color = bottom.style.color = "red";
+  } else {
+    top.style.color = bottom.style.color = "black";
+  }
+
+  val.innerHTML = resultCards[1];
 };
 
-buttonRandom.addEventListener("click", () => {
-  generateCard(suits, values);
-});
+buttonRandom.addEventListener("click", generateCard);
 
 window.onload = () => {
-  generateCard(suits, values);
+  generateCard();
 };
